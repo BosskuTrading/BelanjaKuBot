13a298350f2e5d3571f16027504e20cd85140463
BosskuTrading <bosskutrading@gmail.com>
2025-05-17 20:19:52 +0800

    Initial commit - add bot1 and bot2 code

--- bot1_laporbelanja.py
+++ bot1_laporbelanja.py
@@ -254,4 +254,6 @@ async def webhook():
         abort(405)
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
